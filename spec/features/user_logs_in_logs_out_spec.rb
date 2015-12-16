b1180dd4d8006f2000b5190d5cebad75d5086f7f
MaxMillington <maxmillington89@gmail.com>
2015-12-16 14:41:35 -0700

    add user logs in spec

---
+++ spec/features/user_logs_in_logs_out_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'user visits homepage' do
+  context 'logs in' do
+
+    it 'works with valid credentials' do
+
+      User.create(username: 'Steve', password: 'steve')
+      
+      visit root_path
+      click_link 'Login'
+
+      fill_in 'Username', with: 'Steve'
+      fill_in 'Password', with: 'steve'
+      click_button 'Submit'
+
+      expect(current_path).to eq(root_path)
+    end
+
+    it 'doesnt work with invalid credentials' do
+
+      visit root_path
+      click_link 'Login'
+
+      fill_in 'Username', with: 'Chuck'
+      fill_in 'Password', with: 'password'
+      click_button 'Submit'
+
+      expect(page).to have_content('Password or username is incorrect')
+
+    end
+  end
+
+
+end
